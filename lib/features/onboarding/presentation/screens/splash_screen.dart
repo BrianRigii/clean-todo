79cd3a1adfdac7c68b878571d0f31ae57c00c9fd
John Brian <johnbryanjb011@gmail.com>
2023-08-23 15:28:57 +0300

    dashboard screen

--- lib/features/onboarding/presentation/screens/splash_screen.dart
+++ lib/features/onboarding/presentation/screens/splash_screen.dart
@@ -1,8 +1,27 @@
+import 'package:clean_todo/features/dashboard/presentation/home_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
   static const path = '/';
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _init();
+  }
+
+  Future<void> _init() async {
+    await Future.delayed(const Duration(seconds: 2));
+    if (mounted) context.pushReplacement(HomeScreen.path);
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
