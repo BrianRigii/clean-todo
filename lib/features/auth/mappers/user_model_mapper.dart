9f96ac24ac662834e0319e3f07f953142a0b2f8d
John Brian <johnbryanjb011@gmail.com>
2023-09-29 15:59:00 +0300

    user model mappere

--- lib/features/auth/mappers/user_model_mapper.dart
+++ lib/features/auth/mappers/user_model_mapper.dart
@@ -1,6 +1,6 @@
 import 'package:clean_todo/features/auth/data/models/user_model.dart';
+import 'package:clean_todo/features/auth/domain/enitities.dart';
 
-
-extension UserModelMapper on UserModel{
-  User get toEntity => User(id: id, appMetadata: appMetadata, userMetadata: userMetadata, aud: aud, createdAt: createdAt)
+extension UserModelMapper on UserModel {
+  User get toEntity => User(name: name, email: email, id: id, active: active);
 }
